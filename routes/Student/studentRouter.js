65e66a166dac2ab16ff4d9cd04b8e83b28f103bd
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-08-08 23:14:20 +0700

    Update get available student and delete get unbooked student endpoint

--- routes/Student/studentRouter.js
+++ routes/Student/studentRouter.js
@@ -4,10 +4,9 @@ let express = require('express'),
     { validateBody, schemas } = require('./StudentValidation'),
     JWtController = require('../JWT/JWTController');
 
-router.get('/available/:school', JWtController.checkToken, StudentController.getAvailable);
+router.get('/available/:schoolId', JWtController.checkToken, StudentController.getAvailable);
 router.get('/count/:schoolId', JWtController.checkToken, StudentController.count);
 router.get('/school/:schoolId', JWtController.checkToken, StudentController.listBySchool);
-router.get('/unbooked-accommodation/school/:school', JWtController.checkToken, StudentController.getUnbooked);
 router.delete('/:studentId', JWtController.checkToken, StudentController.delete);
 router.post('/', JWtController.checkToken, JWtController.isSchool, validateBody(schemas.create), StudentController.create);
 router.put('/', JWtController.checkToken, validateBody(schemas.update), StudentController.update);
