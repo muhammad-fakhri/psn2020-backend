8a59f6cf16394e4ea321fb10d7ab0bdc2e791dfa
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-08-13 10:57:57 +0700

    Add serve static file configuration

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 require('dotenv').config({ path: __dirname + '/.env' });
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
-let cors = require('cors');
+const indexRouter = require('./routes/index');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const logger = require('morgan');
+const cors = require('cors');
 
-let indexRouter = require('./routes/index');
-
-let app = express();
-let mongoose = require('mongoose');
-let mongoServer = "mongodb://localhost:27017/psn2020"
+const app = express();
+const mongoose = require('mongoose');
+const mongoServer = "mongodb://localhost:27017/psn2020"
 mongoose.connect(mongoServer, { useNewUrlParser: true, autoIndex: false });
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
-app.use(logger('dev'));
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+// serve static and uploaded files
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'uploads')));
+
+// enable files upload
+app.use(fileUpload({
+    createParentPath: true
+}));
+app.use(logger('dev')); // logging HTTP request
+app.use(cors()); // enabling CORS
+app.use(express.json()); // parse request body
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser()); // enabling parse cookie from request
+
 app.use('/', indexRouter);
 
 module.exports = app;
