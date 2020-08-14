eb251b5fdb9ebe773356c2b2808f5c6592e0edda
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-08-15 00:50:43 +0700

    Update configuration for sending email and add .env.copy

--- routes/Mail.js
+++ routes/Mail.js
@@ -1,19 +1,28 @@
 const nodemailer = require('nodemailer');
-const transport = {
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    secure: false, // true for 465, false for other ports
+let environment = process.env;
+
+const SMTPTransporter = nodemailer.createTransport({
+    host: environment.SMTP_SERVICE_HOST,
+    secure: false,
+    port: environment.SMTP_SERVICE_PORT,
     auth: {
-        user: process.env.MAIL_USERNAME,
-        pass: process.env.MAIL_PASSWORD,
-    },
-};
+        user: environment.SMTP_USER_NAME,
+        pass: environment.SMTP_PASSWORD
+    }
+});
+
+const GmailTransporter = nodemailer.createTransport({
+    service: environment.GMAIL_SERVICE_NAME,
+    auth: {
+        user: environment.GMAIL_USER_NAME,
+        pass: environment.GMAIL_USER_PASSWORD
+    }
+});
+
+const transporter = environment.MAIL_SERVICE === 'gmail' ? GmailTransporter : SMTPTransporter;
 
 class Mail {
     static async sendVerifyEmail(name, email, token) {
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport(transport);
-
         // send mail with defined transport object
         let info = await transporter.sendMail({
             from: '"Pesta Sains Nasional 2020" <admin@psn.ipb.ac.id>',
@@ -22,30 +31,38 @@ class Mail {
             text: "Please verify your email",
             html: `
                 <h2>Hello ${name}</h2>
-                <h3>Thank you for registering at PSN 2020<h3> 
+                <h3>Thank you for registering at Pesta Sains Nasional 2020<h3> 
                 <p>Before you can continue please verify your email first by clicking the link below.</p>
-                <a target="_blank" href="${process.env.BASE_URL}/auth/email/verify?email=${email}&token=${token}">Click this link to verify your email</a>
+                <a target="_blank" href="${environment.BASE_URL}/auth/email/verify?email=${email}&token=${token}">Click this link to verify your email</a>
             `,
+        }, function (error, info) {
+            if (error) {
+                console.log(error);
+            }
+            console.log("email is send");
+            console.log(info);
         });
     }
 
     static async sendForgotPasswordEmail(name, email, token) {
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport(transport);
-
         // send mail with defined transport object
         let info = await transporter.sendMail({
             from: '"Pesta Sains Nasional 2020" <admin@psn.ipb.ac.id>',
             to: email,
             subject: "Reset Password",
             text: "You can reset your password by click the link in this email",
-            // TODO: set link to open frontend page for set new password
             html: `
                 <h2>Hello ${name}</h2>
                 <h3>You are requesting a password reset<h3> 
                 <p>You can reset your password by clicking the link below</p>
-                <a target="_blank" href="${process.env.BASE_URL}?email=${email}&token=${token}">Click this link to reset your password</a>
+                <a target="_blank" href="${environment.FRONT_END_URL}/password/reset?email=${email}&token=${token}">Click this link to reset your password</a>
             `,
+        }, function (error, info) {
+            if (error) {
+                console.log(error);
+            }
+            console.log("email is send");
+            console.log(info);
         });
     }
 }
