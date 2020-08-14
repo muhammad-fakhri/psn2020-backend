2517fa989c261e3933fce6aea2563d2dd2f0ce83
Muhamad Hilmy Haidar <hilmy_21@apps.ipb.ac.id>
2020-08-14 21:16:56 +0700

    remove school check when create team

--- routes/Team/teamRouter.js
+++ routes/Team/teamRouter.js
@@ -1,17 +1,36 @@
-let express = require('express'),
-    router = express.Router(),
-    TeamController = require('./TeamController'),
-    { validateBody, schemas } = require('./TeamValidation'),
-    JWtController = require('../JWT/JWTController');
+let express = require("express"),
+	router = express.Router(),
+	TeamController = require("./TeamController"),
+	{ validateBody, schemas } = require("./TeamValidation"),
+	JWtController = require("../JWT/JWTController");
 
-router.get('/count', JWtController.checkToken, JWtController.isAdmin, TeamController.count);
-router.get('/excel/contest/:contestId', JWtController.checkToken, JWtController.isAdmin, TeamController.getExcelByContest);
-router.get('/:teamId', JWtController.checkToken, TeamController.get);
-router.delete('/:teamId', JWtController.checkToken, TeamController.delete);
-router.get('/', JWtController.checkToken, TeamController.list);
-router.post('/', JWtController.checkToken, JWtController.isSchool, validateBody(schemas.create), TeamController.create);
-router.put('/', JWtController.checkToken, validateBody(schemas.update), TeamController.update);
+router.get(
+	"/count",
+	JWtController.checkToken,
+	JWtController.isAdmin,
+	TeamController.count
+);
+router.get(
+	"/excel/contest/:contestId",
+	JWtController.checkToken,
+	JWtController.isAdmin,
+	TeamController.getExcelByContest
+);
+router.get("/:teamId", JWtController.checkToken, TeamController.get);
+router.delete("/:teamId", JWtController.checkToken, TeamController.delete);
+router.get("/", JWtController.checkToken, TeamController.list);
+router.post(
+	"/",
+	JWtController.checkToken,
+	validateBody(schemas.create),
+	TeamController.create
+);
+router.put(
+	"/",
+	JWtController.checkToken,
+	validateBody(schemas.update),
+	TeamController.update
+);
 // router.get('/unpaid/school/:school', JWtController.checkToken, TeamController.getUnpaid);
-
 
 module.exports = router;
