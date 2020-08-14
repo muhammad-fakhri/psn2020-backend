5ee1437c86434775c9f72cae38ef140dfb5873c5
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-08-14 14:17:28 +0700

    Update function for create new payment

--- routes/Payment/PaymentController.js
+++ routes/Payment/PaymentController.js
@@ -12,6 +12,7 @@ let mongoose = require('mongoose');
 // let cid = '00298', sck = '787b175aeb54a1e133fb71b5d2ebe11d'; // credential dev
 let cid = '00773', sck = '61c16a7e0dab54a0709ad748f485951e'; // credential prod
 let ParamModel = require('../Params/ParamModel');
+
 class PaymentController {
     static async getAllPayment(req, res) {
         try {
@@ -23,106 +24,49 @@ class PaymentController {
     }
 
     static async create(req, res, next) {
-        try { //only for school
-            let { type, school } = req.value.body,
+        try {
+            let { school, teams } = req.value.body,
                 totalPrice = 0,
                 cid = '00773',
                 sck = '61c16a7e0dab54a0709ad748f485951e',
-                teams = null,
-                teachers = null,
-                students = null,
-                bookings = null,
-                numberOfStudent = 0,
-                numberOfTeacher = null,
-                trx_id = mongoose.Types.ObjectId(),
-                lastVA = await ParamModel.findOne({ code: "LAST_VA" }),
-                firstVA = Math.floor(1000 + Math.random() * 9000),
-                virtual_account = "98800773" + firstVA.toString() + lastVA.value,
-                nextValue = (parseInt(lastVA.value) + 1).toString();
+                trx_id = mongoose.Types.ObjectId();
+
+            // Generate virtual account
+            let lastVA = await ParamModel.findOne({ code: "LAST_VA" });
+            let firstVA = Math.floor(1000 + Math.random() * 9000);
+            let virtual_account = "98800773" + firstVA.toString() + lastVA.value;
+            nextValue = (parseInt(lastVA.value) + 1).toString();
             lastVA.value = "0".repeat(4 - nextValue.length) + nextValue;
             lastVA.save();
-            console.log(virtual_account);
-            school = await SchoolModel.findById({ _id: school });
-            if (type == 'registration') {
-                teams = await TeamModel.find({ school, isPaid: { $ne: true } }).populate({ path: 'contest' }); //TeamController.findBySchool(school._id,{path: 'contest'});
-                teachers = await TeacherModel.find({ school: school._id, isPaid: { $ne: true } });//TeacherController.findBySchool(school._id);
-                numberOfTeacher = teachers.length;
-                if (teams.length == 0 && teachers.length == 0)
-                    return res.status(400).json({ message: "Tidak ada tim atau guru pendamping yang belum dibayar. Silahkan tambahkan tim atau guru pendamping baru.", bill: null });
-                let totalStudent = await StudentModel.count({ school: school._id }),
-                    totalTeacher = await TeacherModel.count({ school: school._id });
-                if (teams.length != 0) {
-                    for (let i = 0; i < teams.length; i++) {
-                        let price = parseInt(teams[i].contest.memberPerTeam) * parseInt(teams[i].contest.pricePerStudent);
-                        console.log(40, price, teams[i].contest.memberPerTeam, teams[i].contest.pricePerStudent);
-                        totalPrice += price;
-                        numberOfStudent += teams[i].contest.memberPerTeam;
-                        [i].isPaid = true;
-                        await teams[i].save();
-                    }
-                }
-                console.log(46, totalPrice);
-                if (teachers.length != 0) {
-                    let jatahMurah, jatahMurahDipakai, jatahMurahSisa, bayarMahal, jatahMurahBakalDipakai;
-                    if (totalStudent > 0 && totalStudent <= 5) {
-                        jatahMurah = 1;
-                    } else if (totalStudent > 5 && totalStudent <= 15) {
-                        jatahMurah = 2;
-                    } else if (totalStudent > 15 && totalStudent <= 30) {
-                        jatahMurah = 3;
-                    } else {
-                        jatahMurah = 4;
-                    }
-                    jatahMurahDipakai = (totalTeacher - numberOfTeacher >= jatahMurah ? jatahMurah : totalTeacher - numberOfTeacher);
-                    jatahMurahSisa = jatahMurah - jatahMurahDipakai;
-                    jatahMurahBakalDipakai = (numberOfTeacher >= jatahMurahSisa ? jatahMurahSisa : numberOfTeacher);
-                    bayarMahal = (numberOfTeacher - jatahMurahSisa < 0 ? 0 : numberOfTeacher - jatahMurahSisa);
-                    totalPrice += jatahMurahBakalDipakai * 50000 + bayarMahal * 100000;;
-                    for (let i = 0; i < numberOfTeacher; i++) {
-                        teachers[i].isPaid = true;
-                        await teachers[i].save();
-                    }
-                }
-                console.log(87, totalPrice);
-            } else if (type == 'accommodation') {
-                /**
-                 * Disini proses penghitungan tagihan penginapan.
-                 * 1. Ambil data booking berdasarkan sekolah
-                 * 2. Hitung jumlah biaya per orang berdasarkan accommodation
-                 * 3. POST ke bank
-                 * 4. simpan ke DB
-                 */
-                bookings = await BookingModel.find({ school, isFinal: false }).populate('accommodation');
-                // console.log(bookings);
-                teachers = [];
-                students = [];
-                for (let i = 0; i < bookings.length; i++) {
-                    console.log(totalPrice, bookings[i].accommodation.pricePerNight, bookings[i].duration);
-                    totalPrice += bookings[i].accommodation.pricePerNight * bookings[i].duration; // Kurang atribut durasi booking
-                    bookings[i].isFinal = true;
-                    bookings[i].save();
-                    if (bookings[i].userType == 'teacher') {
-                        teachers.push(bookings[i].teacher);
-                    }
-                    else {
-                        students.push(bookings[i].student);
-                    }
-                }
-                // return res.json({bookings,totalPrice});
-            } else {
-                throw new Error('invalid bill type');
+
+            console.log('VANumber : ' + virtual_account);
+
+            // get school data
+            let schoolData = await SchoolModel.findById({ _id: school });
+
+            // calculate price
+            if (teams.length == 0) return res.status(400).json({ message: "No team ID provided" });
+            for (let index = 0; index < teams.length; index++) {
+                let teamData = await TeamModel.findById(teams[index]).populate('contest');
+                let price = teamData.students.length * parseInt(teamData.contest.pricePerStudent);
+
+                console.log('price for team : ', teamData.name, price, teamData.students.length, teamData.contest.pricePerStudent);
+
+                totalPrice += price;
+                teamData.isFinal = true;
+                await teamData.save();
             }
-            console.log(108, totalPrice, virtual_account);
-            totalPrice += 5000; //Biaya admin
-            // totalPrice += Math.floor(Math.random()*(899)+100);
-            // throw new Error(totalPrice);
+
+            // Administration fee
+            totalPrice += 5000;
+
             let data = {
                 type: "createbilling",
                 client_id: cid,
                 trx_id,
                 trx_amount: totalPrice,
                 billing_type: "c",
-                customer_name: school.name,
+                customer_name: schoolData.name,
                 virtual_account
             }
             let encryptedData = PaymentEncription.encrypt(data, cid, sck),
@@ -136,34 +80,24 @@ class PaymentController {
                         data: encryptedData
                     }
                 });
-            console.log(request, data, request.data);
-            let bill = null; // result = request.data.data, decryptedData = PaymentEncription.decrypt(result.data,cid,sck);
 
-            // console.log(data);
-            if (type == 'registration') {
-                bill = await PaymentModel.create({
-                    _id: trx_id, type, totalPrice, VANumber: virtual_account,
-                    payment: { status: 'waiting' }, school, registration:
-                    {
-                        teams, teachers, numberOfStudent: numberOfStudent, numberOfTeacher: numberOfTeacher
-                    }
-                });
-            }
-            else if (type == 'accommodation') {
-                bill = await PaymentModel.create({
-                    _id: trx_id, type, totalPrice, VANumber: virtual_account,
-                    payment: { status: 'waiting' }, school, accommodation:
-                    {
-                        teachers: teachers, students: students, bookings
-                    }
-                });
-                // return res.json({bill,students, teachers})
-            }
-            return res.status(201).json({ bill });
+            // create payment
+            let payment = await PaymentModel.create({
+                _id: trx_id,
+                totalPrice,
+                VANumber: virtual_account,
+                school,
+                teams,
+                status: 'waiting',
+                createdDate: Date.now()
+            });
+
+            return res.status(201).json({ payment });
         } catch (e) {
-            res.status(400).json({ message: e.message });
+            res.status(500).json({ message: e.message });
         }
     }
+
     static async callback(req, res, next) {
         // urus lagi nanti, update bill ke database
         try {
