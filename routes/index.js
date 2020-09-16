02552b5d0edbd2eff4666f01a110d0a4ffa7b934
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-09-16 17:32:08 +0700

    Update test

--- routes/index.js
+++ routes/index.js
@@ -27,9 +27,13 @@ router.use("/teams", TeamRouter);
 router.use("/payment", PaymentRouter);
 router.post("/test", function (req, res, next) {
   let { name, role } = req.body;
+  let host = req.get("host");
+  let origin = req.get("origin");
   return res.status(200).json({
     name,
     role,
+    host,
+    origin,
     message: "Masuk datanya",
   });
 });
