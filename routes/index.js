0ffbf75e4958d7e65c6aa975b32aef4383ebc55c
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-09-16 17:12:58 +0700

    Add test route

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const AuthRouter = require('./Auth/authRouter');
-const AdminRouter = require('./Admin/adminRouter');
-const StudentRouter = require('./Student/studentRouter');
-const ContestRouter = require('./Contest/contestRouter');
-const TeamRouter = require('./Team/teamRouter');
-const SchoolRouter = require('./School/schoolRouter');
-const ParamRouter = require('./Params/ParamRouter');
-const PaymentRouter = require('./Payment/PaymentRouter');
+const AuthRouter = require("./Auth/authRouter");
+const AdminRouter = require("./Admin/adminRouter");
+const StudentRouter = require("./Student/studentRouter");
+const ContestRouter = require("./Contest/contestRouter");
+const TeamRouter = require("./Team/teamRouter");
+const SchoolRouter = require("./School/schoolRouter");
+const ParamRouter = require("./Params/ParamRouter");
+const PaymentRouter = require("./Payment/PaymentRouter");
 
 // API Front Page Index
-router.get('/', function (req, res, next) {
-  return res.status(200).json({ message: "Welcome to Pesta Sains Nasional 2020 API. Check the documentation for more information" });
+router.get("/", function (req, res, next) {
+  return res.status(200).json({
+    message:
+      "Welcome to Pesta Sains Nasional 2020 API. Check the documentation for more information",
+  });
 });
-router.use('/auth', AuthRouter);
-router.use('/admin', AdminRouter);
-router.use('/contest', ContestRouter);
-router.use('/params', ParamRouter);
-router.use('/schools', SchoolRouter);
-router.use('/school', SchoolRouter);
-router.use('/students', StudentRouter);
-router.use('/teams', TeamRouter);
-router.use('/payment', PaymentRouter);
+router.use("/auth", AuthRouter);
+router.use("/admin", AdminRouter);
+router.use("/contest", ContestRouter);
+router.use("/params", ParamRouter);
+router.use("/schools", SchoolRouter);
+router.use("/school", SchoolRouter);
+router.use("/students", StudentRouter);
+router.use("/teams", TeamRouter);
+router.use("/payment", PaymentRouter);
+router.post("/test", function (req, res, next) {
+  let { name, role } = req.body;
+  return res.status(200).json({
+    name,
+    role,
+    message: "Masuk datanya",
+  });
+});
 
 module.exports = router;
